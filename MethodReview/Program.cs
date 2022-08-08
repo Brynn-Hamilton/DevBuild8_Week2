330510b12fd31caa2a4ae1b8c7710bfc63a6ee73
Brynn Hamilton <brynnhamilton1@rocketmortgage.com>
2022-08-08 10:27:28 -0400

    Add project files.

---
+++ MethodReview/Program.cs
@@ -0,0 +1,11 @@
+﻿while (true)
+{
+    Console.WriteLine("Please enter an integer.");
+    string entry = Console.ReadLine();
+
+    int num;
+
+    bool result = int.TryParse(entry, out num);
+    Console.WriteLine(result);
+    Console.WriteLine(num);
+}
